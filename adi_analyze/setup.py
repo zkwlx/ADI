2c659ac16c7cb7745db3c4b0fc92d17f17979c53
zkw <zkwlixue@163.com>
2019-10-13 22:57:59 +0800

    修改 analyzer 工程结构

--- adi_analyze/setup.py
+++ adi_analyze/setup.py
@@ -8,16 +8,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='adi_analyzer',
-    version='0.9.3',
+    version='0.9.0',
     author='zkwlx',
     author_email='zkwlixue@163.com',
     url='https://github.com/zkwlx/ADI',
     description='A powerful tool to help Android engineers optimize their performance',
-    packages=find_packages().append("adi_analyzer.py"),
+    packages=find_packages(),
     install_requires=['bokeh'],
     entry_points={
         'console_scripts': [
-            'adi_analyzer=adi_analyzer:main'
+            'adi_analyzer=main.analyzer:main'
         ]
-    }
+    },
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3 :: Only',
+    ],
 )
